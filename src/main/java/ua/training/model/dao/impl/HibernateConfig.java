eb8973936d7f8192d4ce307b91468c4de6f3024a
OleksandrZabudskyi <alexjavaw@gmail.com>
2018-07-17 15:54:15 +0300

    add simple hibernate config to mysql

---
+++ src/main/java/ua/training/model/dao/impl/HibernateConfig.java
@@ -0,0 +1,42 @@
+package ua.training.model.dao.impl;
+
+import com.mysql.jdbc.Driver;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.MySQL5Dialect;
+import ua.training.util.PropertyLoader;
+
+import java.util.Properties;
+
+import static ua.training.constant.Config.*;
+
+public class HibernateConfig {
+    private static volatile SessionFactory sessionFactory;
+
+    private static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            synchronized (ConnectionPoolHolder.class) {
+                if (sessionFactory == null) {
+                    Properties properties = PropertyLoader.load(DB_CONFIG);
+                    Configuration config = new Configuration()
+                            .setProperty(Environment.DRIVER, Driver.class.getCanonicalName())
+                            .setProperty(Environment.DIALECT, MySQL5Dialect.class.getCanonicalName())
+                            .setProperty(Environment.URL, properties.getProperty(DB_URL))
+                            .setProperty(Environment.USER, properties.getProperty(DB_USER))
+                            .setProperty(Environment.PASS, properties.getProperty(DB_PASSWORD))
+                            .setProperty(Environment.HBM2DDL_AUTO, "validate")
+                            .setProperty(Environment.SHOW_SQL, "true");
+                    sessionFactory = config.buildSessionFactory();
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static Session getSession() {
+        return getSessionFactory().openSession();
+    }
+
+}
